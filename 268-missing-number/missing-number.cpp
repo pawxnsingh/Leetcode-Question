fb2a4725094a1653096a9c20c15ff8e234111c25
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-09-08 08:38:38 +0530

    Time: 14 ms (73.07%) | Memory: 18 MB (14.27%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,21 +4,21 @@ public:
         int n = nums.size();
 
         int i = 0;
-        while(i<nums.size())
+        while (i < n)
         {
-            while(nums[i] < n && nums[i] != i)
-            {
-                swap(nums[i],nums[nums[i]]);
+            if (nums[i] < n && nums[i] != i){
+                swap(nums[i], nums[nums[i]]);
             }
-
-            i++;
+            else{
+                i++;
+            }
         }
 
         for(int i=0;i<n;i++)
         {
-            if(nums[i]!=i)
-                return i;
+            if(nums[i] != i) return i;
         }
         return n;
+
     }
 };
