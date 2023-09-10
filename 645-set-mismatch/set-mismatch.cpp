6c874090097d3eaf96604d720701b4fe47228621
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-09-10 10:01:46 +0530

    Time: 24 ms (74.53%) | Memory: 21.4 MB (81.05%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         int i = 0;
         while(i<nums.size())
+        // [1,2,3,4,7,6,6,5] best test case written by me
         {   
             int correct = nums[i] - 1;
             if(nums[i] != nums[correct])
