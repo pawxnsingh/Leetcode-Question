d1623d949ea41daeeaec51e9ebec8e7e07d2adad
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2023-11-26 10:18:18 +0530

    Time: 106 ms (20.31%) | Memory: 42.5 MB (53.43%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const ans = [];
+    for(let i=0;i<nums.length-1;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i] + nums[j] === target){
+                ans.push(i);
+                ans.push(j);
+            }
+        }
+    }
+    return ans;
+};
