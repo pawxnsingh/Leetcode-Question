0736121898f56155edc3dba80d954aa9ec6d7890
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2024-04-27 10:37:28 +0530

    Time: 6 ms (34.51%) | Memory: 10.7 MB (30.18%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        // this will iterate through to all the person <P>
-        vector<int> arr(101, 0);
-        for (int i = 0; i < logs.size(); i++) {
-            // the second loop is for the iterate through birthand death ranges
-            for (int j = logs[i][0]; j < logs[i][1]; j++) {
-                arr[j - 1950]++;
+        // create an array of 2055 size as per the constaint
+        vector<int> v(2051, 0);
+
+        // traverse through the array mark the starting index +1
+        // and ending index with -1
+        for (auto it : logs) {
+            v[it[0]]++;
+            v[it[1]]--;
+        }
+
+        int max = 0, year = 0;
+        // calculate the prefix sum of the array
+        for (int i = 1; i < 2051; i++) {
+            v[i] += v[i - 1];
+            if (max < v[i]) {
+                max = v[i];   
+                year = i;
             }
         }
-        int maxPop = 0;
-        int maxYear = 1950;
-        for (int i = 0; i < 101; i++) {
-            if (arr[i] > maxPop) {
-                maxPop = arr[i];
-                maxYear = i + 1950;
-            }
-        }
-        return maxYear;
+        return year;
     }
 };
