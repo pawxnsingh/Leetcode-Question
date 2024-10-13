630024c7cd16a9f19e79bf45825d1fd8fc25fb60
Pawan Singh Dogra <48354037+pawxnsingh@users.noreply.github.com>
2024-10-13 10:58:52 +0530

    Time: 3 ms (29.70%) | Memory: 7.7 MB (9.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    // this is memoized solution // n = 5, and so i make the array of size 6 cuz
-    // 0 is also there
-    int calculateNumberOfDistictWays(int n, vector<int>& dp) {
-        if (n == 1 || n == 0) {
-            return 1;
-        }
-
-        if(dp[n] != -1) return dp[n];
-
-        return dp[n] = calculateNumberOfDistictWays(n - 1, dp) +
-               calculateNumberOfDistictWays(n - 2, dp);
-    }
-
+    // this is the tabular form
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
-        return calculateNumberOfDistictWays(n, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
